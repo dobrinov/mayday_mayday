11df1773f9bd056e938a956b21f1a5f3365aafe2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-11-29 12:40:17 +0200

    Show data only from the last hour till time interval selectors are implemented

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -1,6 +1,7 @@
 class MeasurementsController < ApplicationController
   def index
     @metrics = Metric.where(layer: params[:layer])
-    @measurements = Measurement.where(layer: params[:layer], name: params[:name]).page(params[:page])
+    @measurements = Measurement.where('created_at > ?', Time.now - 1.hour)
+                               .where(layer: params[:layer], name: params[:name]).page(params[:page])
   end
 end
