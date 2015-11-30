8487a2cd811bdb730e636f74b485c988b4ad74b2
Deyan Dobrinov <deyan.dobrinov@gmail.com>
2015-12-01 01:17:45 +0200

    Implement simple charts

--- app/controllers/measurements_controller.rb
+++ app/controllers/measurements_controller.rb
@@ -2,6 +2,13 @@ class MeasurementsController < ApplicationController
   def index
     @metrics = Metric.where(layer: params[:layer])
     @measurements = Measurement.where('created_at > ?', Time.now - 1.hour)
-                               .where(layer: params[:layer], name: params[:name]).page(params[:page])
+                               .where(layer: params[:layer], name: params[:name])
+                               .order(created_at: :asc)
+
+    if params[:chart]
+      @series = @measurements.group_by(&:host)
+    else
+      @measurements = @measurements.page(params[:page])
+    end
   end
 end
